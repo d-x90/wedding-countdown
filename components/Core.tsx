d3826e0866ccea8b6cc561242ffb5ae3dc0fb6ea
d-x90 <work.dhorvath@gmail.com>
2022-01-13 19:05:46 +0100

    Improve styling

--- components/Core.tsx
+++ components/Core.tsx
@@ -1,39 +1,58 @@
-import Atropos from 'atropos/react';
-import { useRef } from 'react';
-import style from '../styles/Core.module.scss'
-import Counter from './Counter';
+import Atropos from "atropos/react";
+import { useRef } from "react";
+import style from "../styles/Core.module.scss";
+import Counter from "./Counter";
 
 const dateOfWedding = new Date("Sat, 18 Jun 2022 00:00:00 UTC+1");
 
 const Core = () => {
-    const text = useRef(null)
+  const text = useRef(null);
 
-    return (<>
-        <div className={style.core}>
-            <Atropos 
-                shadow={false}
-                activeOffset={100}
-                className={style.myAtropos}
-                onEnter={() => text && text.current ? text.current.style.transform = 'scale(1.3) translateX(-15px)' : null}
-                onLeave={() => text && text.current ? text.current.style.transform = 'scale(1.0) translateX(0)' : null}
-            >
-                <div className={style.movingContainer}>
-                    <div className={style.movingFlowers}>
-                        <span className={style.invitationTitle}>MEGHÍVÓ</span>
-                        <Counter date={dateOfWedding}></Counter>
-                        <span className={style.signoNames}>Jenni&Dávid</span>
-                        <span className={style.weddingDate}>2022.06.18</span>
-                        <a 
-                            className={style.weddingLocation}
-                            href='https://www.google.com/maps/d/u/0/viewer?ie=UTF8&om=0&source=embed&oe=UTF8&msa=0&mid=1axWYGF4vqs-lKb0smDE60AGFnt8&ll=47.38945899999998%2C18.387487&z=17'
-                            target="_blank"
-                            rel="noreferrer"
-                        >Gánt</a>
-                    </div>
-                </div>
-            </Atropos>
-        </div>
-    </>);
-}
+  return (
+    <>
+      <div className={style.core}>
+        <Atropos
+          shadow={false}
+          activeOffset={100}
+          className={style.myAtropos}
+          onEnter={() =>
+            text && text.current
+              ? (text.current.style.transform = "scale(1.3) translateX(-15px)")
+              : null
+          }
+          onLeave={() =>
+            text && text.current
+              ? (text.current.style.transform = "scale(1.0) translateX(0)")
+              : null
+          }
+        >
+          <div className={style.movingContainer}>
+            <div className={style.movingFlowers}>
+              <span data-atropos-offset="9" className={style.invitationTitle}>
+                MEGHÍVÓ
+              </span>
+              <Counter date={dateOfWedding}></Counter>
+              <span data-atropos-offset="6" className={style.signoNames}>
+                Jenni&Dávid
+              </span>
+              <span data-atropos-offset="9" className={style.weddingDate}>
+                2022.06.18
+              </span>
+              <a
+                data-atropos-offset="8"
+                className={style.weddingLocation}
+                href="https://www.google.com/maps/d/u/0/viewer?ie=UTF8&om=0&source=embed&oe=UTF8&msa=0&mid=1axWYGF4vqs-lKb0smDE60AGFnt8&ll=47.38945899999998%2C18.387487&z=17"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Gánt
+              </a>
+            </div>
+          </div>
+        </Atropos>
+      </div>
+    </>
+  );
+};
 
 export default Core;
--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,51 +1,62 @@
 import { useEffect, useState } from "react";
-import style from "../styles/Counter.module.scss" 
+import style from "../styles/Counter.module.scss";
 
 const ONE_DAY_IN_MILIS = 24 * 60 * 60 * 1000;
 const ONE_HOUR_IN_MILIS = 60 * 60 * 1000;
 const ONE_MINUTE_IN_MILIS = 60 * 1000;
 const ONE_SECOND_IN_MILIS = 1000;
 
-const Counter = ({date}) => {
-    const [counterString, setCounterString] = useState("00:00:00:00");
+const Counter = ({ date }) => {
+  const [counterString, setCounterString] = useState("00:00:00:00");
 
-    useEffect(() => {
-        const intervalHandler = setInterval(() => {
-            let days = 0;
-            let hours = 0;
-            let minutes = 0;
-            let seconds = 0;
+  useEffect(() => {
+    const intervalHandler = setInterval(() => {
+      let days = 0;
+      let hours = 0;
+      let minutes = 0;
+      let seconds = 0;
 
-            let diff = date.getTime() - Date.now();
-    
-            if(diff >= ONE_DAY_IN_MILIS) {
-                days = Math.floor(diff / ONE_DAY_IN_MILIS);
-                diff -= days * ONE_DAY_IN_MILIS;
-            }
-    
-            if(diff >= ONE_HOUR_IN_MILIS) {
-                hours = Math.floor(diff / ONE_HOUR_IN_MILIS);
-                diff -= hours * ONE_HOUR_IN_MILIS;
-            }
-    
-            if(diff >= ONE_MINUTE_IN_MILIS) {
-                minutes = Math.floor(diff / ONE_MINUTE_IN_MILIS);
-                diff -= minutes * ONE_MINUTE_IN_MILIS;
-            }
-    
-            if(diff >= ONE_SECOND_IN_MILIS) {
-                seconds = Math.floor(diff / ONE_SECOND_IN_MILIS);
-                diff -= seconds * ONE_SECOND_IN_MILIS;
-            }
-    
-            setCounterString(`${new String(days).padStart(2, '0')}:${new String(hours).padStart(2, '0')}:${new String(minutes).padStart(2, '0')}:${new String(seconds).padStart(2, '0')}`)
-        }, 1000);
-        return () => {
-            clearInterval(intervalHandler);
-        }
-    }, [date])
+      let diff = date.getTime() - Date.now();
 
-    return <div className={style.counter}>{counterString}</div>
-}
+      if (diff >= ONE_DAY_IN_MILIS) {
+        days = Math.floor(diff / ONE_DAY_IN_MILIS);
+        diff -= days * ONE_DAY_IN_MILIS;
+      }
+
+      if (diff >= ONE_HOUR_IN_MILIS) {
+        hours = Math.floor(diff / ONE_HOUR_IN_MILIS);
+        diff -= hours * ONE_HOUR_IN_MILIS;
+      }
+
+      if (diff >= ONE_MINUTE_IN_MILIS) {
+        minutes = Math.floor(diff / ONE_MINUTE_IN_MILIS);
+        diff -= minutes * ONE_MINUTE_IN_MILIS;
+      }
+
+      if (diff >= ONE_SECOND_IN_MILIS) {
+        seconds = Math.floor(diff / ONE_SECOND_IN_MILIS);
+        diff -= seconds * ONE_SECOND_IN_MILIS;
+      }
+
+      setCounterString(
+        `${new String(days).padStart(2, "0")}:${new String(hours).padStart(
+          2,
+          "0"
+        )}:${new String(minutes).padStart(2, "0")}:${new String(
+          seconds
+        ).padStart(2, "0")}`
+      );
+    }, 1000);
+    return () => {
+      clearInterval(intervalHandler);
+    };
+  }, [date]);
+
+  return (
+    <div data-atropos-offset="7" className={style.counter}>
+      {counterString}
+    </div>
+  );
+};
 
 export default Counter;
